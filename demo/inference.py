955701a49ff63869670ab1a31ab14e9f949d40e1
imunellka <107074158+imunellka@users.noreply.github.com>
2025-04-20 12:54:42 +0300

    Update inference.py

--- demo/inference.py
+++ demo/inference.py
@@ -1,3 +1,5 @@
+import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import numpy as np
@@ -11,7 +13,6 @@ from models.TransformerbasedEncoder import TransformerbasedEncoder
 from models.WaterSystemAnomalyTrasformer import WaterSystemAnomalyTrasformer
 import json
 import torch.nn.functional as F
-import os
 
 def transform_anomaly_scores(X_attack, scores):
     y_pred = np.zeros(X_attack.shape[0])
