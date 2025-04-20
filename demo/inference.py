6155252d3085f62e4e35943d19294f6597a94c5e
imunellka <107074158+imunellka@users.noreply.github.com>
2025-04-20 13:13:50 +0300

    Update inference.py

--- demo/inference.py
+++ demo/inference.py
@@ -27,7 +27,7 @@ def transform_anomaly_scores(X_attack, scores):
 def load_model(model_path, device="cpu"):
     model = Autoencoder()
     print(model_path)
-    model.load_state_dict(torch.load(model_path, weights_only=True, map_location=device))
+    model.load_state_dict(torch.load(model_path, weights_only=True, map_location=torch.device('cpu')))
     model.to(device)
     model.eval()
 
